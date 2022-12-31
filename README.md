a3b2c43764f1a25078a30af594f3098babd56342
Jacopo <work.jacopocomparin@gmail.com>
2022-12-31 17:45:23 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+I hate having to figure out screen size by "inches".
+
+I want to know if the monitor i'm buying fits on my desk not how many hamburgers per bald eagle its diagonal is
---
+++ src/main.js
@@ -0,0 +1,45 @@
+const INCH_TO_CM_K = 2.35
+
+
+function on_valueschange()
+{
+	let d = +document.getElementById("in-d-inches").value
+	let ar = getAspectRatio()
+	setDimensions( calculateDimensions( d, ar[0], ar[1] ) )
+}
+
+
+/**
+ * @param {number} size the size of the screen in inches
+ * @param {number} rw aspect ratio width
+ * @param {number} rh aspect ratio heigth
+ * @returns {number[]} the dimensions in inches [x,y,unit]
+ */
+function calculateDimensions(size, rw, rh)
+{
+	let Dpow = Math.pow(size, 2)
+	let unit = Math.sqrt(Dpow / (rw * rw + rh * rh))
+	return [unit * rw, unit * rh, unit]
+}
+
+
+function getAspectRatio()
+{
+	let w = +document.getElementById("in-ratio-w").value
+	let h = +document.getElementById("in-ratio-h").value
+	return [w, h]
+}
+
+/**
+ * 
+ * @param {number} precision 
+ * @param  {number[]} whu 
+ */
+function setDimensions(whu, precision = 1)
+{
+	let prec10 = Math.pow(10, precision)
+	document.getElementById("out-size-w-cm").value = Math.round(whu[0] * INCH_TO_CM_K * prec10 + Number.EPSILON) / prec10
+	document.getElementById("out-size-h-cm").value = Math.round(whu[1] * INCH_TO_CM_K * prec10 + Number.EPSILON) / prec10
+	document.getElementById("out-size-w-in").value = Math.round(whu[0] * prec10 + Number.EPSILON) / prec10
+	document.getElementById("out-size-h-in").value = Math.round(whu[1] * prec10 + Number.EPSILON) / prec10
+}
